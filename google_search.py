ec55dbc3583a1bbb2c62fea8082150aeb9d6b0d1
goyal15rajat <goyal.rajat15@gmail.com>
2017-02-01 16:59:15 +0530

    update
    remove count variable and added version specification

--- google_search.py
+++ google_search.py
@@ -1,10 +1,11 @@
+#developer - rajat
+#version - python3
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import urllib.request
 import re
 
-import numpy as np
-count=0
+#Forming query
 query=input("query>>")
 query=query.strip().split()
 query="+".join(query)
@@ -24,4 +25,5 @@ for item in soup.find_all('h3', attrs={'class' : 'r'}):
     line = (reg.match(item.a['href'][7:]).group())
     links.append(line[:-4])
 
+#printing links scrapped as a list
 print(links)
